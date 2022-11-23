bf90e018f0fccd5405d367a62871103739453a7f
Amjad <support@techtrivandrum.com>
2022-11-23 02:24:17 -0800

    changes to db file

--- db-back.php
+++ db-back.php
@@ -13,12 +13,9 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-?>
+// echo "Connected successfully";
 
-
-
-echo "Reached here";
+// echo "Reached here";
 
 
 ?>
--- db.php
+++ db.php
@@ -1,9 +1,17 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "Pass@123";
-$database = "php_crud";
+
+getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $servername="localhost";
+getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port="3306";
+getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $username="root";
+getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $password="Pass@123";
+getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $database="php_crud";
+
+
+//$servername = "localhost";
+//$username = "root";
+//$password = "Pass@123";
+//$database = "php_crud";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -13,7 +21,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-// echo "Connected successfully";
+  echo "Connected successfully";
 
 // echo "Reached here";
 
